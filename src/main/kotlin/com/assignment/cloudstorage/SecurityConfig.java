468f54f8462dcc20ae8f78d9638721bc0eb10770
amire <amirezasobhdel@gmail.com>
2022-01-16 23:01:37 +0330

    security config added

--- src/main/kotlin/com/assignment/cloudstorage/SecurityConfig.java
+++ src/main/kotlin/com/assignment/cloudstorage/SecurityConfig.java
@@ -1,12 +1,16 @@
 package com.assignment.cloudstorage;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.header.writers.StaticHeadersWriter;
 import org.springframework.web.cors.CorsConfiguration;
 
 import java.util.List;
 
+@Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter
 {
@@ -14,14 +18,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.setAllowedHeaders(List.of("*"));
-        corsConfiguration.setAllowedOrigins(List.of("*"));
-        corsConfiguration.setAllowedMethods(List.of("*"));
-        corsConfiguration.setAllowCredentials(true);
-
-        // You can customize the following part based on your project, it's only a sample
-        http.cors().configurationSource(request -> corsConfiguration);
+        http
+                // your security config here
+                .authorizeRequests()
+                .anyRequest().permitAll()
+                .and().httpBasic()
+                .and().headers().frameOptions().disable()
+                .and().csrf().disable()
+                .headers()
+                // the headers you want here. This solved all my CORS problems! 
+                .addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-Origin", "*"))
+                .addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-Methods", "*"))
+                .addHeaderWriter(new StaticHeadersWriter("Access-Control-Max-Age", "*"))
+                .addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-Credentials", "true"))
+                .addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-Headers", "*"));
 
     }
 }
