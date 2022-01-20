181727e925491073fe1dc36d44698ccaa59eedeb
amire <amirezasobhdel@gmail.com>
2022-01-20 12:34:17 +0330

    download added

--- src/main/kotlin/com/assignment/cloudstorage/controllers/FileController.kt
+++ src/main/kotlin/com/assignment/cloudstorage/controllers/FileController.kt
@@ -5,10 +5,19 @@ import com.assignment.cloudstorage.services.AuthenticationService
 import com.assignment.cloudstorage.services.FileService
 import com.assignment.cloudstorage.services.UserService
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.multipart.MultipartFile
+import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody
+import java.io.FileNotFoundException
+import java.io.IOException
+import java.io.OutputStream
+import java.io.RandomAccessFile
+import java.nio.file.Files
+import java.nio.file.Paths
+import java.security.Principal
 
 @CrossOrigin
 @RestController
@@ -63,17 +72,98 @@ class FileController {
         return ResponseEntity(HttpStatus.UNAUTHORIZED)
     }
 
-    @GetMapping("/{fileId}")
-    fun getFile(@CookieValue(value = "username") username: String,
-                   @CookieValue(value = "password") password: String,
-                   @PathVariable("fileId") fileId: String): ResponseEntity<FileEntity>
-    {
+//    @GetMapping("/{fileId}")
+//    fun getFile(@CookieValue(value = "username") username: String,
+//                   @CookieValue(value = "password") password: String,
+//                   @PathVariable("fileId") fileId: String): ResponseEntity<FileEntity>
+//    {
+//        authenticationService.authenticate(username, password)?.let { user ->
+//
+//            val file = fileService.provideFile(fileId)
+//            println(file)
+//
+//            return ResponseEntity(file, HttpStatus.OK)
+//        }
+//        return ResponseEntity(HttpStatus.UNAUTHORIZED)
+//    }
+
+
+    @GetMapping("{fileId}")
+    fun downloadFile(@PathVariable fileId: String,
+                     @RequestHeader(value = "Range", required = false) rangeHeader: String?,
+                     @CookieValue(value = "username") username: String,
+                     @CookieValue(value = "password") password: String
+    ): ResponseEntity<StreamingResponseBody?>? {
         authenticationService.authenticate(username, password)?.let { user ->
 
-            val file = fileService.provideFile(fileId)
-            println(file)
-
-            return ResponseEntity(file, HttpStatus.OK)
+            val fileEntity = fileService.provideFile(fileId)
+            return try {
+                val responseStream: StreamingResponseBody
+                val filePathString = fileEntity!!.getFilePath()
+                val filePath = Paths.get(filePathString)
+                val fileSize = Files.size(filePath)
+                val buffer = ByteArray(1024)
+                val responseHeaders = HttpHeaders()
+                if (rangeHeader == null) {
+                    responseHeaders.add("Content-Type", fileEntity.getContentType())
+                    responseHeaders.add("Content-Length", fileSize.toString())
+                    responseStream = StreamingResponseBody { os: OutputStream ->
+                        val file = RandomAccessFile(filePathString, "r")
+                        try {
+                            file.use {
+                                var pos: Long = 0
+                                file.seek(pos)
+                                while (pos < fileSize - 1) {
+                                    file.read(buffer)
+                                    os.write(buffer)
+                                    pos += buffer.size.toLong()
+                                }
+                                os.flush()
+                            }
+                        } catch (e: Exception) {
+                        }
+                    }
+                    return ResponseEntity(responseStream, responseHeaders, HttpStatus.OK)
+                }
+                val ranges = rangeHeader.split("-".toRegex()).toTypedArray()
+                val rangeStart = ranges[0].substring(6).toLong()
+                var rangeEnd: Long
+                rangeEnd = if (ranges.size > 1) {
+                    ranges[1].toLong()
+                } else {
+                    fileSize - 1
+                }
+                if (fileSize < rangeEnd) {
+                    rangeEnd = fileSize - 1
+                }
+                val contentLength = (rangeEnd - rangeStart + 1).toString()
+                responseHeaders.add("Content-Type", fileEntity.getContentType())
+                responseHeaders.add("Content-Length", contentLength)
+                responseHeaders.add("Accept-Ranges", "bytes")
+                responseHeaders.add("Content-Range", "bytes $rangeStart-$rangeEnd/$fileSize")
+                val _rangeEnd = rangeEnd
+                responseStream = StreamingResponseBody { os: OutputStream ->
+                    val file = RandomAccessFile(filePathString, "r")
+                    try {
+                        file.use {
+                            var pos = rangeStart
+                            file.seek(pos)
+                            while (pos < _rangeEnd) {
+                                file.read(buffer)
+                                os.write(buffer)
+                                pos += buffer.size.toLong()
+                            }
+                            os.flush()
+                        }
+                    } catch (e: Exception) {
+                    }
+                }
+                ResponseEntity(responseStream, responseHeaders, HttpStatus.PARTIAL_CONTENT)
+            } catch (e: FileNotFoundException) {
+                ResponseEntity(HttpStatus.NOT_FOUND)
+            } catch (e: IOException) {
+                ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR)
+            }
         }
         return ResponseEntity(HttpStatus.UNAUTHORIZED)
     }
